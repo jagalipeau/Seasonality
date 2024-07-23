7ab4916d01d1c4618b3eb0761cf7edd98a5f9c64
Joe Galipeau <joejoe4223@gmail.com>
2024-07-23 11:00:44 -0600

    removed unused test code

--- avg_returns_list.py
+++ avg_returns_list.py
@@ -7,32 +7,6 @@ from pandas.errors import SettingWithCopyWarning
 warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
 
 
-stocks = ["AAPL", "CHWY", "DJT", "AMZN", "TSLA", "F", "BABA", "V", "WMT", "JPM"]
-
-'''
-# This function takes in stock data and a number of years. It calculates the average return for each year.
-def get_stock_data(stock_data, years=5):
-    """
-    Calculate the average return for each year given the stock data.
-
-    Parameters:
-    stock_data (type): The ticker data for the stocks.
-    years (int): The number of years to calculate the average return for. Default is 5.
-    Returns:
-    average returns for each year.
-    """
-    try:
-        if years < 1:
-            return pd.DataFrame()
-        else:
-            # Calculate the average return using the avg_eturn function.
-            rtn = avg_rtn(stock_data, years)
-            return rtn
-    except:
-        get_stock_data(stock_data, years - 1)
-'''
-
-
 def get_monthly_returns(stocks_list, month):
     returns = []
 
@@ -57,8 +31,3 @@ def get_monthly_returns(stocks_list, month):
     df = pd.concat(returns, ignore_index=True).drop("month_no", axis=1)
     df.set_index("Ticker", inplace=True)
     return df
-
-
-# Call the function and print the result
-#
-# print(get_monthly_returns(stocks, "June"))
